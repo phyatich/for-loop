ae4cb9fa1195d902bf70c30f3d6501969660a11f
Fatih Ozdemir <fatih.zdemr@gmail.com>
2022-02-05 03:24:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Bir sayı giriniz: ");
+            // int counter = int.Parse(Console.ReadLine());
+            // for (int i = 0; i <= counter; i++)
+            // {
+            //     if (i % 2 == 1)
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            // }
+
+            // int oddSum = 0;
+            // int evenSum = 0;
+
+            // for (int i = 0; i < 1000; i++)
+            // {
+            //     if (i % 2 == 1)
+            //     {
+            //         oddSum += i;
+            //     }
+            //     else
+            //     {
+            //         evenSum += i;
+            //     }
+            // }
+
+            // Console.WriteLine("Tek Toplam: " + oddSum);
+            // Console.WriteLine("Çift Toplam: " + evenSum);
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
